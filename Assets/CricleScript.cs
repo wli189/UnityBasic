6885d3399170777c3c94d23a6db93f57b5b3e9a6
Brian <brianleelwt@gmail.com>
2024-02-29 16:32:25 -0600

    first commit

---
+++ Assets/CricleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CricleScript : MonoBehaviour
+{
+    Renderer m_Renderer;
+    Rigidbody m_Rigidbody;
+    int threshhold = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateColor();
+        if (GameObject.Find("Cube").GetComponent<SquareScript>().shiftCount % 5 == threshhold) {
+            m_Rigidbody.AddForce(new Vector3(0f, 4f, 0f), ForceMode.Force);
+        }
+    }
+
+    void UpdateColor() {
+        m_Renderer.material.color = GameObject.Find("Cube").GetComponent<SquareScript>().GetColor();
+    }
+}
---
+++ Assets/SquareScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareScript : MonoBehaviour
+{
+    Color[] ColorWheel = {Color.black, Color.blue, Color.clear, Color.cyan, Color.gray, Color.green, Color. magenta, Color.red, Color.white, Color.yellow};
+    Renderer m_Renderer;
+    public int shiftCount  = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color randColor;
+        if (Input.GetKeyDown("z")) {
+            randColor = ColorWheel[Random.Range(0, 9)];
+            Debug.Log("the random color is: " + randColor);
+            m_Renderer.material.color = randColor;
+            shiftCount++;
+        }
+    }
+
+    public Color GetColor() {
+        return m_Renderer.material.color;
+    }
+}
